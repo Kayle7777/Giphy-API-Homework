e598badce9aba3480efe7dd44b8c1b69cd3d18f9
Kayle7777 <jessewebb2@gmail.com>
2018-07-11 14:14:30 -0500

    Added stop / play functionality for all the generated button gifs. Now I just need to put in some bootstrap / css styling and I'm done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,27 @@
-WebsiteObject = {
+let WebsiteObject = {
   buttons: ["Dog","Cat","Fish","Bunny"],
   giphyArray: [],
   renderButtons: function() {
     $("#buttons").empty();
     this.giphyArray = [];
+// ==================================================================================================================
+    // I should probably use a constructor here for giphyArray, because all this map does is build giphy array into a new array of objects
     this.buttons.map((element, index) => {
       $.get(`http://api.giphy.com/v1/gifs/search?q=${element}&rating=g&api_key=hB3OC11Wjmrbng5YPx9vRogIa5OsMSxH&limit=15`)
       .then((response) => {
-        let obj = {}
+        let obj = {};
         obj.name = element;
-        obj.images = [];
+        obj.images_animated = [];
+        obj.images_still = [];
         response.data.map((x, i) => {
           obj.rating = x.rating;
-          obj.images.push(x.images.fixed_width.url);
+          obj.images_animated.push(x.images.fixed_width.url);
+          obj.images_still.push(x.images.fixed_width_still.url);
         })
         this.giphyArray.push(obj);
         })
       $("#buttons").append(`<button class="button" name='${element}'>${element}</button>`);
     })
-
   }
 }
 
@@ -35,25 +38,30 @@ $(document).on("click", ".button", function() {
   $("#giphyGifs").empty();
   WebsiteObject.giphyArray.map((arr) => {
     if (arr.name == this.name) {
-      arr.images.map((link) => {
-        let container = $("<div>"), rating = $("<p>"), gif = $("<img>");
-        gif.attr("class", ".gif");
+      arr.images_still.map((link, index) => {
+        let container = $("<div>"), rating = $("<p>"), gif = $("<img>"), button = $("<button>");
+        button.attr("class", ".gif");
         gif.attr("src", link);
-        gif.attr("data-state", still);
+        gif.attr("data-state", "still");
+// ==================================================================================================================
+        // This attaches the function that changes each button to its animated / still version
+        button.click(function() {
+          let imgInfo = $(this).children()[0];
+          if (imgInfo.dataset.state == "still") {
+            imgInfo.dataset.state = "animate";
+            imgInfo.src = arr.images_animated[index];
+          } else {
+            imgInfo.dataset.state = "still";
+            imgInfo.src = arr.images_still[index];
+          }
+        })
+// ==================================================================================================================
+        button.append(gif);
         rating.text(`rating: ${arr.rating}`);
         container.append(rating);
-        container.append(gif);
+        container.append(button);
         $("#giphyGifs").append(container);
       })
     }
   })
 })
-
-$(".gif").on("click"), () => {
-  let state = $(this).attr("data-state");
-  let url = $(this).attr("src");
-  console.log(url)
-  if (state == "still") {
-    
-  }
-}

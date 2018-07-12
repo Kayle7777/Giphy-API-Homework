f7445a8e433ffe4b2771cc5c0c45cb890c34a1d2
Kayle7777 <jessewebb2@gmail.com>
2018-07-12 06:14:08 -0500

    Added a favorites saver thing, still need to bootstrap all this shiz up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,14 @@ let WebsiteObject = {
     this.giphyArray = [];
 // ==================================================================================================================
     // I should probably use a constructor here for giphyArray, because all this map does is build giphy array into a new array of objects
-    this.buttons.map((element, index) => {
+    this.buttons.map((element) => {
       $.get(`http://api.giphy.com/v1/gifs/search?q=${element}&rating=g&api_key=hB3OC11Wjmrbng5YPx9vRogIa5OsMSxH&limit=15`)
       .then((response) => {
         let obj = {};
         obj.name = element;
         obj.images_animated = [];
         obj.images_still = [];
-        response.data.map((x, i) => {
+        response.data.map((x) => {
           obj.rating = x.rating;
           obj.images_animated.push(x.images.fixed_width.url);
           obj.images_still.push(x.images.fixed_width_still.url);
@@ -39,13 +39,13 @@ $(document).on("click", ".button", function() {
   WebsiteObject.giphyArray.map((arr) => {
     if (arr.name == this.name) {
       arr.images_still.map((link, index) => {
-        let container = $("<div>"), rating = $("<p>"), gif = $("<img>"), button = $("<button>");
-        button.attr("class", ".gif");
+        let container = $("<div>"), rating = $("<p>"), gif = $("<img>"), giphyButton = $("<button>"), favButton = $("<button>");
+        giphyButton.attr("class", ".gif");
         gif.attr("src", link);
         gif.attr("data-state", "still");
 // ==================================================================================================================
-        // This attaches the function that changes each button to its animated / still version
-        button.click(function() {
+        // This attaches the function that changes each giphyButton to its animated / still version
+        function giphyAnimate() {
           let imgInfo = $(this).children()[0];
           if (imgInfo.dataset.state == "still") {
             imgInfo.dataset.state = "animate";
@@ -54,12 +54,24 @@ $(document).on("click", ".button", function() {
             imgInfo.dataset.state = "still";
             imgInfo.src = arr.images_still[index];
           }
+        }
+
+        giphyButton.click(giphyAnimate)
+
+        favButton.text("\u2729")
+        favButton.click(function() {
+          let faves = $("#faves");
+          let test = $(this).parent()[0]
+          let star = $(this).parent().children()[2]
+          star.remove()
+          faves.append(test)
         })
 // ==================================================================================================================
-        button.append(gif);
+        giphyButton.append(gif);
         rating.text(`rating: ${arr.rating}`);
         container.append(rating);
-        container.append(button);
+        container.append(giphyButton);
+        container.append(favButton);
         $("#giphyGifs").append(container);
       })
     }

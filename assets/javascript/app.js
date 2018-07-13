5c7943305b0401a3444096f2a9e737b115fe82c0
Kayle7777 <jessewebb2@gmail.com>
2018-07-13 09:09:53 -0500

    Faves button working nicely

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,11 +65,14 @@ $(document).on("click", ".button", function() {
         // This is for the star / favorites button
         favButton.click(function() {
           let faves = $("#faves");
-          let test = $(this).parent()[0];
+          let buttonContainer = $(this).parent()[0];
+          let button = $(this).parent().children()[1]
           let star = $(this).parent().children()[2];
           let paragraph = $(this).parent().children()[0];
-          let fixer = $("<div class='gifContainer col-md-3'>");
-          fixer.append(test);
+          $(button).removeClass('col-md-10');
+          $(button).addClass('col-md-12');
+          let fixer = $("<div class='gifContainer col-md-3 btn-group'>");
+          fixer.append(buttonContainer);
           star.remove();
           paragraph.innerHTML = '';
           faves.append(fixer);
